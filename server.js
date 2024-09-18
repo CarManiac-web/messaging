2849e6d51b77f5fed30ba870729de6e2be97d63f
Car <janetmatta81@gmail.com>
2024-09-17 20:06:59 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-    const socket = new WebSocket('wss://localhost:8080'); // Ensure this is the correct URL
+    const socket = new WebSocket('ws://localhost:8080'); // Ensure this is the correct URL
 
     socket.onmessage = function(event) {
         const messagesDiv = document.getElementById('messages');
